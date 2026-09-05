5dc2e1424bed65295eb8a68cd92b5a67cac50879
triptyvarma2-dotcom <triptyvarma2@gmail.com>
2026-09-05 23:20:15 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,33 +1,60 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-      // m= nums1.length;
-       // n=nums2.length;
-        int temp[]=new int[m+n];
-        int k=0;
-        int i=0;
-        int j=0;
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                temp[k]=nums1[i];
-                i++;
-            }else{
-                temp[k]=nums2[j];
-                j++;
-            }
-            k++;
+     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        n=nums2.length;
+       m= nums1.length-n;
+         
+//         int temp[]=new int[m+n];
+//         int k=0;
+//         int i=0;
+//         int j=0;
+//         while(i<m && j<n){
+//             if(nums1[i]<=nums2[j]){
+//                 temp[k]=nums1[i];
+//                 i++;
+//             }else{
+//                 temp[k]=nums2[j];
+//                 j++;
+//             }
+//             k++;
+//         }
+//         while(i<m){
+//             temp[k]=nums1[i];
+//             i++;
+//             k++;
+//         }
+//         while(j<n){
+//             temp[k]=nums2[j];
+//             j++;
+//             k++;
+//         }
+//         for(int l=0;l<nums1.length;l++){
+//             nums1[l]=temp[l];
+//         }
+//     }
+// }
+int i=m-1;
+int j=n-1;
+int k=m+n-1;
+while(i>=0 && j>=0){
+     if(nums1[i]>= nums2[j]){
+            nums1[k]=nums1[i];
+            i--;
+        }else{
+            nums1[k]=nums2[j];
+            j--;
         }
-        while(i<m){
-            temp[k]=nums1[i];
-            i++;
-            k++;
-        }
-        while(j<n){
-            temp[k]=nums2[j];
-            j++;
-            k++;
-        }
-        for(int l=0;l<nums1.length;l++){
-            nums1[l]=temp[l];
-        }
+        k--;
     }
+
+// while(i>=0){
+//     nums1[k]=nums1[i];
+//     i--;
+//     k--;
+// }
+while(j>=0){
+    nums1[k]=nums2[j];
+    j--;
+    k--;
+}
+     }
 }
