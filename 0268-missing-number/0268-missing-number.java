954b311bdf302c520ee60a49aa6215a3c0741fb8
triptyvarma2-dotcom <triptyvarma2@gmail.com>
2026-09-08 00:32:21 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.84%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1=0;
+        int xor2=0;
+        for(int i =0;i<nums.length;i++){
+            xor1=xor1^(i+1);
+            xor2=xor2^nums[i];
+        }
+        return xor1^xor2;
+    }
+}
