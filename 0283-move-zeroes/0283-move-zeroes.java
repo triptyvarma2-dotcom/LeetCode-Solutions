e19508a13f6149802a80e5079b3e807e1b84e2af
triptyvarma2-dotcom <triptyvarma2@gmail.com>
2026-09-17 01:20:14 +0530

    Time: 5 ms (7.98%), Space: 47.7 MB (57.44%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[i]==0&& nums[j]!=0){
+                nums[i]=nums[j];
+                nums[j]=0;
+                i++;
+            }
+            if(nums[i]!=0){
+                i++;
+            }
+        }
+        System.out.println(nums);
+
+    }
+}
